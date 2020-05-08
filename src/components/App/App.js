dfec056aba3996fb2412e594f244153ef0b456cd
Pavan Kumar Patruni <pavankumar.patruni@publicissapient.com>
2020-05-09 03:52:01 +0530

    image added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+
+import Icon from '../../../assets/images/bg.png';
+
 import './App.scss';
 
 const App = () => {
-  return <div>React App</div>;
+  return (
+    <div>
+      <h1>React App</h1>
+      <img src={Icon} alt="ICON" />
+    </div>
+  );
 };
 
 export default App;
